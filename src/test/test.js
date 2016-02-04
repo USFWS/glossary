660ac75ad5e4d84fe2e260cffcfbe21f3569c6ae
Roy Hewitt <roy_hewitt@fws.gov>
2016-02-04 13:20:54 -0500

    Created source directory and made sure test works

---
+++ src/test/test.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+
+  var expect = require('chai').expect;
+
+  describe('My first test', function() {
+    it('should return true', function () {
+      expect(true).to.be.true;
+    });
+  });
+})();
