8e0e1ac0d11a31be1da5101374afbc3c55c2af9a
Roy Hewitt <roy_hewitt@fws.gov>
2016-02-04 13:38:04 -0500

    Rename test to glossary.js

--- src/test/glossary.js
+++ src/test/glossary.js
@@ -2,6 +2,7 @@
   'use strict';
 
   var expect = require('chai').expect;
+  var glossary = require('../js/index.js');
 
   describe('My first test', function() {
     it('should return true', function () {
