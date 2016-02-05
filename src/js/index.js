b56867de56f92dcd5941e8a8adf795d7039f21bc
Roy Hewitt <roy_hewitt@fws.gov>
2016-02-05 18:33:41 -0500

    Pass lunrjs index function into glossary init function

--- src/js/index.js
+++ src/js/index.js
@@ -4,13 +4,22 @@
   var data = require('./get-terms');
   var emitter = require('./mediator');
 
+  var lunrIndex = function () {
+    this.field('name', { boost: 10 });
+    this.field('description');
+    this.field('related', { boost: 5 });
+    this.field('acronym', { boost: 3 });
+    this.ref('id');
+  };
+
   data.load('../data/terms.js');
 
   emitter.on('terms:loaded', function (terms) {
 
     glossary.init({
       terms: terms,
-      container: '.glossary-container'
+      container: '.glossary-container',
+      lunrIndex: lunrIndex
     });
 
   });
