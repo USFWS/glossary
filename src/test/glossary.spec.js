e2ee35c3dc64c8dbd498f6bfb0456a0283e1c99d
Roy Hewitt <roy_hewitt@fws.gov>
2016-02-05 17:54:39 -0500

    Add tests for glossary

--- src/test/glossary.spec.js
+++ src/test/glossary.spec.js
@@ -3,12 +3,33 @@
   'use strict';
 
   var expect = require('chai').expect;
+
   var glossary = require('../js/glossary.js');
+  var terms = require('./mocks/terms.json');
 
   describe('The glossary', function() {
-    it('should be inactive when it initializes', function () {
-      glossary.init();
-      expect(glossary.isActive()).to.be.false;
+
+    beforeEach(function () {
+      glossary.init({
+        terms: terms
+      });
+    });
+
+    afterEach(function () {
+      glossary.remove();
+    });
+
+    describe('upon initialization', function () {
+
+      it('should be inactive', function () {
+        expect(glossary.isActive()).to.be.false;
+      });
+
+      it('should render one list item per term', function () {
+        var listItems = glossary.getOption('list').querySelectorAll('.glossary-term');
+        expect(terms.length).to.equal(listItems.length);
+      });
+
     });
   });
 })();
