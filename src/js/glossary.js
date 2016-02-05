c961f0c0b8725009ff01eca6035791a88c321845
Roy Hewitt <roy_hewitt@fws.gov>
2016-02-05 17:57:08 -0500

    Create element, expose options for testing

--- src/js/glossary.js
+++ src/js/glossary.js
@@ -1,24 +1,42 @@
 (function () {
   'use strict';
   var _ = require('./util')._;
-  var xhr = require('xhr');
+  var dom = require('./util').dom;
+  var emitter = require('./mediator');
+  var template = require('../templates/term.jade');
 
   var options = {};
   var defaults = {
     active: false,
-    termClass: 'glossary-term',
-    content: 'main-content'
+    target: document.body,
+    position: 'right',
+    terms: []
   };
 
   function init(opts) {
     options = _.defaults(opts, defaults);
-    if (options.dataUrl) downloadTerms();
+    createGlossary();
+    registerHandlers();
+    render();
   }
 
-  function downloadTerms() {
-    xhr.get(options.dataUrl, function (err, res) {
-      if (err) console.error(err);
-      options.terms = JSON.parse(res.body);
+  function createGlossary() {
+    if (!_.isNode(options.target))
+      throw new Error('You provided an invalid DOM node to append the container.');
+
+    options.container = dom.create('aside', '.glossary-container', options.target);
+    options.input = dom.create('input', '.glossary-search', options.container);
+    options.list = dom.create('ol', '.glossary-list', options.container);
+
+    options.target.appendChild(options.container);
+  }
+
+  function registerHandlers() {
+  }
+
+  function render() {
+    options.list.innerHTML = template({
+      terms: options.terms
     });
   }
 
@@ -26,6 +44,19 @@
     return options.active;
   }
 
+  function remove() {
+    // Remove the glossary and all event listeners from the page
+    dom.remove(options.container);
+  }
+
+  function getOption(name) {
+    if (options[name]) return options[name];
+    else throw new Error('Option does not exist');
+  }
+
   exports.init = init;
   exports.isActive = isActive;
+  exports.remove = remove;
+
+  exports.getOption = getOption;
 })();
