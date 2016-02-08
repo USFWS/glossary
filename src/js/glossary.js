7c5952456d0e668224702e7c8921fd157a9198e9
Roy Hewitt <roy_hewitt@fws.gov>
2016-02-08 10:58:19 -0500

    Add close button, toggle event handler

--- src/js/glossary.js
+++ src/js/glossary.js
@@ -12,6 +12,8 @@
   var defaults = {
     active: false,
     target: document.body,
+    toggleClass: 'glossary-toggle',
+    closeText: 'Close',
     position: 'right',
     terms: [],
     minLength: 2
@@ -26,11 +28,15 @@
   }
 
   function createGlossary() {
+    var position = 'glossary-' + options.position;
     if (!_.isNode(options.target))
       throw new Error('You provided an invalid DOM node to append the container.');
 
     options.container = dom.create('aside', 'glossary-container', options.target);
+    dom.addClass(options.container, position);
     options.input = dom.create('input', 'glossary-search', options.container);
+    options.close = dom.create('button', options.toggleClass, options.container);
+    options.close.innerHTML = options.closeText;
     options.list = dom.create('ol', 'glossary-list', options.container);
     options.target.appendChild(options.container);
   }
@@ -49,11 +55,13 @@
 
   function registerHandlers() {
     options.input.addEventListener('keyup', searchKeyup);
+    document.body.addEventListener('click', toggleGlossary);
   }
 
   function destroy() {
     // Remove the glossary and all event listeners from the page
     options.input.removeEventListener('keyup', searchKeyup);
+    document.body.removeEventListener('click', toggleGlossary);
     dom.remove(options.container);
   }
 
@@ -70,18 +78,15 @@
     if (value.length === 0) render(options.terms);
     if (value.length < options.minLength) return;
 
-    search(value).forEach(function(hit) {
+    index.search(value).forEach(function(hit) {
       filtered.push(options.terms[hit.ref]);
     });
     render(filtered);
   }
 
-  function search(query) {
-    return index.search(query);
-  }
-
-  function isActive() {
-    return options.active;
+  function toggleGlossary(e) {
+    var isGlossaryTrigger = dom.hasClass(e.target, options.toggleClass);
+    if (isGlossaryTrigger) toggle();
   }
 
   function getOption(name) {
@@ -89,8 +94,21 @@
     else throw new Error('Option [' + name + '] does not exist');
   }
 
+  function show() {
+    options.active = true;
+    dom.addClass(options.container, 'active');
+  }
+
+  function hide() {
+    options.active = false;
+    dom.removeClass(options.container, 'active');
+  }
+
+  function toggle() {
+    options.active ? hide() : show(); // jshint ignore:line
+  }
+
   exports.init = init;
-  exports.isActive = isActive;
   exports.destroy = destroy;
 
   exports.getOption = getOption;
