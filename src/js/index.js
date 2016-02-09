8a4654ce537967d2162b9380828e82baf8bb67d9
Roy Hewitt <roy_hewitt@fws.gov>
2016-02-09 16:30:51 -0500

    Add event listener for highlighted terms

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,10 @@
 (function () {
   'use strict';
   var glossary = require('./glossary');
+  var highlighter = require('./highlighter');
   var data = require('./get-terms');
   var emitter = require('./mediator');
+  var dom = require('./util').dom;
 
   var lunrIndex = function () {
     this.field('name', { boost: 10 });
@@ -12,9 +14,18 @@
     this.ref('id');
   };
 
+  var highlightClass = 'open-glossary';
+
+  document.body.addEventListener('click', toggleGlossary);
+
   data.load('../data/terms.js');
 
   emitter.on('terms:loaded', function (terms) {
+
+    var words = [];
+    terms.forEach( function (term) {
+      words.push(term.name);
+    });
 
     glossary.init({
       terms: terms,
@@ -22,6 +33,19 @@
       lunrIndex: lunrIndex
     });
 
+    highlighter.init({
+      terms: words,
+      content: document.querySelector('.content'),
+      class: highlightClass
+    });
+
   });
+
+  function toggleGlossary(e) {
+    if (dom.hasClass(e.target, highlightClass)) {
+      glossary.setValue(e.target.textContent);
+      glossary.show();
+    }
+  }
 
 })();
